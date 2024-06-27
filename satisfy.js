991b8a05521ad4d3fc2946b4843ab414aeaee416
HEAMIN1011 <mintwo20@nate.com>
2024-06-27 10:58:11 +0900

    정의, 만족도조사

---
+++ satisfy.js
@@ -0,0 +1,10 @@
+
+
+
+// function insertContentsMax(spanID, max, formNm) {
+// 	if(formNm.value.length > max){
+// 		alert("제한 글자수 "+max+"자를 초과하였습니다.");
+// 		formNm.value = formNm.value.substr(0,max);
+// 	}
+// 	document.getElementById(spanID).innerHTML = formNm.value.length;
+// }
